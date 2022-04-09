a6fb63473ca66318b9ece6ed0347888dc895a953
DancingSnow <1121149616@qq.com>
2022-04-09 13:50:05 +0800

    build(KHLD): version 0.0.3 build

--- khldaemon/constants/core_constant.py
+++ khldaemon/constants/core_constant.py
@@ -3,8 +3,8 @@ NAME_SHORT = 'KHLD'
 NAME = 'KHLDaemon'
 PACKAGE_NAME = 'khldaemon'
 
-VERSION = '0.0.2'       # semver (1.2.3-alpha.4)
-VERSION_PYPI = '0.0.2'  # pythonic ver (1.2.3a4)
+VERSION = '0.0.3'       # semver (1.2.3-alpha.4)
+VERSION_PYPI = '0.0.3'  # pythonic ver (1.2.3a4)
 
 GITHUB_URL = 'https://github.com/DancingSnow0517/KHLDaemon'
 GITHUB_API_LATEST = 'https://api.github.com/repos/DancingSnow0517/KHLDaemon/releases/latest'
